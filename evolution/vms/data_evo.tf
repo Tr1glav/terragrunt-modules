856df8901824505964e8317ef19b57e8deec97e1
myumukhametzyanov <myumukhametzyanov@gmail.com>
2026-06-12 09:21:14 +0300

    fx

--- evolution/vms/data_evo.tf
+++ evolution/vms/data_evo.tf
@@ -38,6 +38,10 @@ locals {
     for sg in try(data.cloudru_evolution_compute_security_group_collection.datasource_security_group.security_groups, []) :
     sg.name => sg
   }
+  subnet_by_cidr = {
+    for subnet in try(data.cloudru_evolution_compute_subnet_collection.datasource_subnet.subnets, []) :
+    subnet.subnet_address => subnet
+  }
   required_images = [
     var.required_image
   ]
@@ -45,7 +49,7 @@ locals {
     for image_name in local.required_images : image_name =>
     try(one([
       for s in data.cloudru_evolution_compute_image_collection.image_collection.images :
-      s if s.display_name == image_name
+      s if s.display_name == image_name.subnet_address
     ]), null)
   }
 }
