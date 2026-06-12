b4991683e35123fa7473a6121c5167077cbd905b
myumukhametzyanov <myumukhametzyanov@gmail.com>
2026-06-12 09:22:37 +0300

    fx

--- evolution/vms/variables.tf
+++ evolution/vms/variables.tf
@@ -35,6 +35,10 @@ variable "public_key" {
   type = string
 }
 
+variable "subnet" {
+  type = string
+}
+
 variable "required_image" {
   description = "default image for vms"
   type        = string
